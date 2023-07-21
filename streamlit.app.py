e46dc11c9ef00c70bbcbf4f49840b3fa30936546
mmkn47 <109784477+mmkn47@users.noreply.github.com>
2023-07-21 12:05:37 +1000

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -4,7 +4,7 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
-streamlit.title('My Mom's New Healthy Diner')
+streamlit.title("My Mom's New Healthy Diner")
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
